1f5c7ec0de0f97b691cd25d9f10fcfa368ca3b99
coachteacher <79242397+coachteacher@users.noreply.github.com>
2021-09-21 13:20:17 -0400

    fix syntax and operation bug

--- script.js
+++ script.js
@@ -56,73 +56,78 @@ window.onload = function(){
 
 //calculator functionality  
 const calculator = document.querySelector('.calculator-body');
-const keys = document.querySelectorAll('.key');
-const operations = document.querySelectorAll('.operator');
-const display = document.querySelector('.number-output');
+          const keys = document.querySelectorAll('.key');
+          const operations = document.querySelectorAll('.operation');
+          const display = document.querySelector('.number-output');
+          let mathArr = [];
 
-keys.forEach(k =>{
-    k.addEventListener('click', e =>{
-      //variables
-      const key = e.target;
-      const keyValue = key.value;
-      const displayValue = display.innerText; 
-      const currentValue = displayValue;
-      let mathArr = [];
+          keys.forEach(k =>{
+              k.addEventListener('click', e =>{
+                //variables
+                const key = e.target;
+                const keyValue = key.value;
+                const displayValue = display.innerText; 
+                const currentValue = displayValue;
+                
 
-      //numbers displayed
-      if(e.target.classList.contains('digit')){
-        if(displayValue === '0'){
-            display.innerText = keyValue;
-        }else {
-            display.innerText = displayValue + keyValue;
-        }
-      }
-      //handle the operators  
-      if(e.target.classList.contains('operator')){ 
-          mathArr.push(displayValue); 
-          mathArr.push(e.target.innerText);
-          console.log(mathArr);
-          display.innerText = keyValue;
-      }
-     
-      if(e.target.classList.contains('delete')){
-        display.innerText = displayValue.substring(0, [displayValue.length -1]);
-      }
+                //numbers displayed
+                if(e.target.classList.contains('digit')){
+                  if(displayValue === '0'){
+                      display.innerText = keyValue;
+                  }else {
+                      display.innerText = displayValue + keyValue;
+                  }
+                }
+                //handle the operators  
+                if(e.target.classList.contains('operator')){ 
+                    mathArr.push(displayValue); 
+                    mathArr.push(e.target.innerText);
+                    console.log(mathArr);
+                    display.innerText = keyValue;
+                }
+               
+                if(e.target.classList.contains('delete')){
+                  display.innerText = displayValue.substring(0, [displayValue.length -1]);
+                }
 
-      if(e.target.classList.contains('reset')){
-        display.innerText = '0';
-        mathArr = [];
-        console.log(mathArr);
-      }
-      
-      if(e.target.classList.contains('equals')){
+                if(e.target.classList.contains('reset')){
+                  display.innerText = '0';
+                  mathArr = [];
+                  console.log(mathArr);
+                }
+                
+                if(e.target.classList.contains('equals')){
                   mathArr.push(displayValue);
                   switch(mathArr[1]){
                     case "+":
-                      let addition = Number(mathArr[0]) + Number(mathArr[2]);
+                      let addition = Number(mathArr[0]) + Number([mathArr.length - 1]);
                       display.innerText = addition;
                       mathArr = [];
                       break;
                       case "-":
-                      let subtraction = Number(mathArr[0]) - Number(mathArr[2]);
+                      let subtraction = Number(mathArr[0]) - Number(mathArr[mathArr.length - 1]);
                       display.innerText = subtraction;
                       mathArr = [];
                       break;
                       case "x":
-                      let multiplication = Number(mathArr[0]) * Number(mathArr[2]);
+                      let multiplication = Number(mathArr[0]) * Number([mathArr.length - 1]);
                       display.innerText = multiplication;
                       mathArr = [];
                       break;
                       case "/":
-                      let division = Number(mathArr[0]) / Number(mathArr[2]);
+                      let division = Number(mathArr[0]) / Number([mathArr.length - 1]);
                       display.innerText = division;
                       mathArr = [];
                       break;
-                  }   
-              }
-      })
-      
-  })
+                  }
+                    
+                }
+                
+
+                })
+                
+
+            })
 
           
 
@@ -134,5 +139,7 @@ keys.forEach(k =>{
 
 
 */
+
+
 
 };
